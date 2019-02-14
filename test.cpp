7e0ebebb3ec5d793572949eecabfce65936d6f05
Victor Wang <vwang0103@gmail.com>
2019-02-13 19:03:36 -0500

    Another another commit

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std
+
+int main()
+{
+  cout << "hello!" << endl << endl;
+  cout << "This is a test message." << endl;
+  system("CLS");
+  cout << "This is a second test message." << endl;
+  system("CLS");
+  return 0;
+}
+/*
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello!" << endl << endl;
+  cout << "This is a test message." << endl;
+  sleep(3);
+  cout << "This is a second test message." << endl;
+  return 0;
+}
+*/
